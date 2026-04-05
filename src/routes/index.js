008df367793e57b91a4fa14f3f1db86f7857c154
Sebastian <sebastevez91@gmail.com>
2026-04-05 11:51:54 -0300

    feat: endpoints CRUD de proveedores

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const authRoutes       = require('./auth.routes');
 const productosRoutes  = require('./productos.routes');
 const categoriasRoutes = require('./categorias.routes');
 const stockRoutes      = require('./stock.routes');
+const proveedoresRoutes = require('./proveedores.routes');
 
 const router = Router();
 
@@ -10,9 +11,9 @@ router.use('/auth', authRoutes);
 router.use('/productos', productosRoutes);
 router.use('/categorias', categoriasRoutes);
 router.use('/stock', stockRoutes);
+router.use('/proveedores', proveedoresRoutes);
 
 // Aquí irán las demás rutas a medida que avances:
-// router.use('/proveedores', proveedoresRoutes);
 // router.use('/compras',     comprasRoutes);
 // router.use('/ventas',      ventasRoutes);
 
---
+++ src/routes/proveedores.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const { getAll, getById, create, update, remove } = require('../controllers/proveedores.controller');
+const { authenticate, authorizeAdmin } = require('../middlewares/auth.middleware');
+
+const router = Router();
+
+router.get('/',       authenticate, getAll);
+router.get('/:id',    authenticate, getById);
+router.post('/',      authenticate, authorizeAdmin, create);
+router.put('/:id',    authenticate, authorizeAdmin, update);
+router.delete('/:id', authenticate, authorizeAdmin, remove);
+
+module.exports = router;

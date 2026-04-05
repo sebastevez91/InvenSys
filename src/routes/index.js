8884706e1ecdd16beb13d49ecd11da7175730626
Sebastian <sebastevez91@gmail.com>
2026-04-05 12:18:29 -0300

    feat: endpoints de compras con detalle y movimientos de stock

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const productosRoutes  = require('./productos.routes');
 const categoriasRoutes = require('./categorias.routes');
 const stockRoutes      = require('./stock.routes');
 const proveedoresRoutes = require('./proveedores.routes');
+const comprasRoutes = require('./compras.routes');
 
 const router = Router();
 
@@ -12,9 +13,9 @@ router.use('/productos', productosRoutes);
 router.use('/categorias', categoriasRoutes);
 router.use('/stock', stockRoutes);
 router.use('/proveedores', proveedoresRoutes);
+router.use('/compras', comprasRoutes);
 
 // Aquí irán las demás rutas a medida que avances:
-// router.use('/compras',     comprasRoutes);
 // router.use('/ventas',      ventasRoutes);
 
 module.exports = router;
